0d657c953300063400af5371a80c6746f6d9f978
nebrassy <nebras30@gmail.com>
2020-08-16 12:24:09 +0300

    sdm660-common: ubuntu: Enable AppArmor
    Signed-off-by: Herobuxx <me@herobuxx.xyz>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,7 @@ BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += selinux=0
 BOARD_KERNEL_CMDLINE += console=tty0
+BOARD_KERNEL_CMDLINE += apparmor=1 security=apparmor
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
